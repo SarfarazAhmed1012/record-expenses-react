1e064292aaa288c12abd0e6472084d30099d2ef8
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2021-09-27 09:45:20 +0500

    another approach for handling the no expenses found (when what the components returns changes entirely, this approach is better

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import '../css/Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react/cjs/react.development';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021');
@@ -16,23 +16,12 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No Expenses Found!</p>
-
-  if (filteredExpenses.length > 0){ // if there are expenses availbale, return the filtered expenses, otherwise return 
-    expensesContent = filteredExpenses.map((expense) => ( // the exact variable 'expensesCount'
-      <ExpenseItem 
-            key = {expense.id}
-            title = {expense.title}
-            amount = {expense.amount}
-            date = {expense.date}  
-          />
-    ));
-  }
+  
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
